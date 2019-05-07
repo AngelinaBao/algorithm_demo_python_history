8ea31c752c778b9c88f2416042f53d3329f1db8f
AngelinaBao <2276913347@qq.com>
2019-05-07 17:23:55 +0800

    Sort Algorithm
    Merge Sort with Pysnooper debug tool;
    Quick Sort

---
+++ Algorithm/Merge_Sort.py
@@ -0,0 +1,26 @@
+import pysnooper
+
+@pysnooper.snoop()
+def merge_sort(a):
+    if len(a) <= 1:
+        return a
+    mid = len(a) // 2
+    left = merge_sort(a[:mid])
+    right = merge_sort(a[mid:])
+    return merge(left, right)
+
+def merge(left, right):
+    i, j = 0, 0
+    result = []
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+print(merge_sort([1, 12, 4, 3, 6, 71, 23, 3]))
---
+++ Algorithm/Quick_Sort.py
@@ -0,0 +1,16 @@
+def quick_sort(data):
+    if len(data) >= 2:
+        mid = data[len(data) - 1]
+        left, right = [], []
+        data.remove(mid)
+        for num in data:
+            if num >= mid:
+                right.append(num)
+            else:
+                left.append(num)
+        return quick_sort(left) + [mid] + quick_sort(right)
+    else:
+        return data
+
+t = [6, 1, 23, 12, 20]
+print(quick_sort(t))
